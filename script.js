07b23a912b8e2ba047f4ca3b449178ebc14a221f
DariaHalchenko <daragalcenko3@gmail.com>
2025-02-12 13:04:22 +0200

    Kasuta jQuery hide/ show meetodid, et saab pilti peida/näita

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ function start() {
     // Pildi lohistamine ja toppimine
     $('#pikachu').draggable();
 }
+// Funktsioonid pildi varjamiseks/näitamiseks
+function peidapilt() {
+    $('#pikachu').hide();
+}
+
+function naitapilt() {
+    $('#pikachu').show();
+}
